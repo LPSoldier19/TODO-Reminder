6c29e58f5390768563b88b8d474038d71daccfdc
Rafael Bautista <rafael.bautista1@hotmail.es>
2022-05-10 22:48:05 -0600

    Almacenamiento de Datos en Local Storage || Creación de mi primer Custom Hook

--- src/App/index.js
+++ src/App/index.js
@@ -3,28 +3,51 @@ import React from "react";
 import { AppUI } from "./AppUI";
 // import './App.css';
 
-const defaultTodos = [
-  {
-    text: 'Cortar cebolla',
-    completed: false
-  },
-  {
-    text: 'Tomar curso de intro a React',
-    completed: false
-  },
-  {
-    text: 'Llorar con la Llorona',
-    completed: true
-  },
-  {
-    text: 'Conseguir lugar para hacer la practica',
-    completed: false
-  },
-];
+// const defaultTodos = [
+//   {
+//     text: 'Cortar cebolla',
+//     completed: false
+//   },
+//   {
+//     text: 'Tomar curso de intro a React',
+//     completed: false
+//   },
+//   {
+//     text: 'Llorar con la Llorona',
+//     completed: true
+//   },
+//   {
+//     text: 'Conseguir lugar para hacer la practica',
+//     completed: false
+//   },
+// ];
+
+function useLocalStorage(itemName, initialValue) {
+
+  const localStorageItem = localStorage.getItem(itemName);
+  let parsedItem;
+
+  if(!localStorageItem) {
+    localStorage.setItem(itemName,JSON.stringify(initialValue));
+    parsedItem = initialValue;
+  } else {
+    parsedItem = JSON.parse(localStorageItem);
+  }
+
+  const [ item , setItem ] = React.useState(parsedItem);
+
+  const saveItem = (newItem) =>{
+    const stringifiedItem = JSON.stringify(newItem);
+    localStorage.setItem(itemName,stringifiedItem);
+    setItem(newItem);
+  }
+
+  return [item, saveItem];
+}
 
 function App(props) {
 
-  const [todos, setTodos] = React.useState(defaultTodos);
+  const [todos, saveTodos] = useLocalStorage('TODOS_V1', []);
   const [searchValue, setSearchValue] = React.useState('');
 
   const completedTodos = todos.filter(todo => !!todo.completed).length;
@@ -48,7 +71,7 @@ function App(props) {
     const todoIndex = todos.findIndex(todo => todo.text === text);
     const newTodos = [...todos];
     newTodos[todoIndex].completed = true;
-    setTodos(newTodos);
+    saveTodos(newTodos);
 
     //Otra opción es la siguiente:
     // todos[todoIndex].completed = {
@@ -61,7 +84,7 @@ function App(props) {
     const todoIndex = todos.findIndex(todo => todo.text === text);
     const newTodos = [...todos];
     newTodos.splice(todoIndex, 1);
-    setTodos(newTodos);
+    saveTodos(newTodos);
   }
 
   return (
